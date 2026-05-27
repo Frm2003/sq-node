da72b9e8b3510b3b9e24abdc75c279e2463dfd4c
Frm2003 <felipper475@gmail.com>
2026-05-27 15:58:00 -0300

    remoção de arquivos e ajustes no entrypoint (index.js)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import DataStorage from './core/DataStorage.js';
-import Select from './dml/Select.js';
-import FileIngestionPipeline from './pipe/FileIngestionPipeline.js';
+import readline from 'readline';
+
+import FileIngestionPipeline from './engine/core/pipe/FileIngestionPipeline.js';
+import Parser from './parser/Parser.js';
 
 await FileIngestionPipeline.run();
 
-console.log(Select.execute({
-    tableName: 'users',
-    fields: ['nome', 'email'],
-    where: [
-        {
-            field: 'nome',
-            operator: '!=',
-            value: null,
-        }
-    ]
-}));
+const start = () => {
+    const ast = Parser.transform(`
+        SELECT profile.*, users.active
+        FROM users
+            JOIN profile ON users.id == profile.users_id
+        WHERE users.active == false AND users.id > 1`
+    );
+};
+
+start();
+
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// function perguntar() {
+//     console.clear();
+    
+//     rl.question(':', (arg) => {
+//         console.clear();
+//         console.log(arg);
+//         Dispatcher.execute(arg);
+//         rl.question('Pressione Enter para continuar...', () => {
+//             perguntar();
+//         });
+//     });
+// }
+
+// perguntar();
